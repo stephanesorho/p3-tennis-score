080a9ae59a7c13832530e3e2110d33a792d8c783
stephanesorho <97770592+stephanesorho@users.noreply.github.com>
2022-11-26 10:51:00 -0800

    Add files via upload

--- db/MyMongoDB.js
+++ db/MyMongoDB.js
@@ -1,31 +1,31 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 function MyMongoDB() {
-    const myDB = {};
-    const url = process.env.MONGO_URL || "mongodb://localhost:27017";
+  const myDB = {};
+  const url = process.env.MONGO_URL || "mongodb://localhost:27017";
 
-    const DB_NAME = "tennisScore";
-    const POINTS_COL = "points";
+  const DB_NAME = "tennisScore";
+  const POINTS_COL = "points";
 
-    myDB.getPoints() = async () => {
-        let client;
+  myDB.getPoints = async () => {
+    let client;
 
-        try {
-            client = new MongoClient(url);
+    try {
+      client = new MongoClient(url);
 
-            //TODO Handle errors
-            const colPoints = client.db(DB_NAME).collection(POINTS_COL);
+      //TODO Handle errors
+      const colPoints = client.db(DB_NAME).collection(POINTS_COL);
 
-            const query = {};  
-            console.log("running query")
-            return await colPoints.find(query).toArray();
-        } finally {
-            console.log("getPoints closing connection")
-            await client.close();
-        }
-    };
+      const query = {};
+      console.log("running query");
+      return await colPoints.find(query).toArray();
+    } finally {
+      console.log("getPoints closing connection");
+      await client.close();
+    }
+  };
 
-    return myDB;
+  return myDB;
 }
 
 export const myDB = MyMongoDB();
